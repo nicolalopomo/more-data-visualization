f8307b3c17535a473b9333da0244bb5ba28e614f
nicolalopomo <nicola.lopomo@polimi.it>
2025-04-02 20:40:43 +0000

    Introduced metric

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Title
+st.title("Streamlit :red[Metric]")
+
+# Display player speed with positive delta value
+st.metric(label="Player **Max Speed**", value="25 km/h", delta="+5.4 km/h")
+
+# Display with a negative delta
+st.metric(label="Player **Ball Possession**", value="24 min", delta="-2 min")
